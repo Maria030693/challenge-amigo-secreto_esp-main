6f32bb989bc31a93d97264404c03b15234491d88
Maria030693 <mariaramirez1993@hotmail.com>
2025-03-17 22:22:23 -0600

    Terminar tarea

---
+++ app.js
@@ -0,0 +1,45 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya ha sido ingresado.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Agregue amigos antes de sortear.");
+        return;
+    }
+    
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo sorteado es: <strong>${amigoSorteado}</strong></li>`;
+}
